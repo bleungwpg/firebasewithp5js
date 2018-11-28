37e96072cf799c25f4ee5329ac21e9e55554cfbf
Ben Leung <benjamin.leung80@gmail.com>
2018-11-28 16:28:40 +0800

    Add files via upload

---
+++ firebaseImageDownloadTester/firebase.js
@@ -0,0 +1,55 @@
+var database;
+var storageRef;
+var theImage;
+var imageReady;
+
+
+function setupFirebase()
+{
+	// Initialize Firebase
+	var config = {
+	apiKey: "AIzaSyBh258HDhov9mIVUjJRiCxNzf4-hmllZWU",
+	authDomain: "p5jstester.firebaseapp.com",
+	databaseURL: "https://p5jstester.firebaseio.com",
+	projectId: "p5jstester",
+	storageBucket: "p5jstester.appspot.com",
+	messagingSenderId: "294054023101"
+	};
+	firebase.initializeApp(config);
+
+	// reference to Real-Time database
+	database = firebase.database();
+
+	// reference to storage
+	storageRef = firebase.storage();
+
+
+}
+
+function downloadImage()
+{
+	// Create a reference to the file we want to download
+	var folderRef = storageRef.ref();
+	var imageRef = folderRef.child('images/present.png');
+	imageReady = false;
+
+	// Get the download URL
+	imageRef.getDownloadURL().then(function(url) {
+		// Insert url into an <img> tag to "download"
+		console.log('log '+url);
+		theImage = loadImage(url);
+		imageReady = true;
+		return 0;
+	});
+
+}
+
+function getImageURL()
+{
+	if (imageReady)
+	{
+		return theImage;
+	}
+
+	return "-1";
+}
---
+++ firebaseImageDownloadTester/sketch.js
@@ -0,0 +1,35 @@
+var p5ImageFile;
+
+function preload()
+{
+}
+
+function setup()
+{
+	createCanvas(600,400);
+	setupFirebase();
+
+	var x = downloadImage();
+	console.log(x);
+
+}
+
+function draw()
+{
+
+	background(0,0,0);
+	fill(125);
+	rect(10,10,100,40);
+	fill(255);
+	text("Show Image",20,30);
+
+	if (mouseX > 10 && mouseX < 10+100 && mouseY > 10 && mouseY < 10 + 40 && mouseIsPressed)
+	{
+		if (getImageURL() != "-1")
+		{
+			p5ImageFile = getImageURL();
+		    image(p5ImageFile,200,50);
+		}
+	}
+}
+
