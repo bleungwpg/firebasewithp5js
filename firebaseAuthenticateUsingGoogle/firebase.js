415e618adf9731a561afc97f4661bdab2d4b197f
Ben Leung <benjamin.leung80@gmail.com>
2018-12-29 20:36:10 +0800

    Add files via upload

--- firebaseAuthenticateUsingGoogle/firebase.js
+++ firebaseAuthenticateUsingGoogle/firebase.js
@@ -49,6 +49,28 @@ function setupFirebase()
 
 function googleSignin()
 {
+	firebase.auth().signInWithRedirect(provider);
+
+	firebase.auth().getRedirectResult().then(function(result) {
+	  if (result.credential) {
+	    // This gives you a Google Access Token. You can use it to access the Google API.
+	    var token = result.credential.accessToken;
+	    // ...
+	  }
+	  // The signed-in user info.
+	  var user = result.user;
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+	  // ...
+	});
+
+	/*
 	firebase.auth().signInWithPopup(provider).then(function(result) {
 	  // This gives you a Google Access Token. You can use it to access the Google API.
 	  var token = result.credential.accessToken;
@@ -69,6 +91,7 @@ function googleSignin()
 	  console.log('error sign in with google');
 	  // ...
 	});
+	*/
 }
 
 function downloadImage()
