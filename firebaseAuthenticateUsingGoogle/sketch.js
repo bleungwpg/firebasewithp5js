5f32421edff59944ff0ad6ef1048a200f9e2a033
Ben Leung <benjamin.leung80@gmail.com>
2018-12-29 20:28:37 +0800

    Add files via upload

---
+++ firebaseAuthenticateUsingGoogle/sketch.js
@@ -0,0 +1,62 @@
+var p5ImageFile;
+var btnSignIn;
+
+function preload()
+{
+}
+
+function setup()
+{
+	createCanvas(600,400);
+	setupFirebase();
+
+	var x = downloadImage();
+	console.log(x);
+
+	btnSignIn = 0;
+
+}
+
+function draw()
+{
+
+	background(0,0,0);
+	fill(125);
+	rect(10,10,100,40);
+	fill(255);
+	text("Show Image",20,30);
+
+	if (mouseX > 10 && mouseX < 10+100 && mouseY > 10 && mouseY < 10 + 40 && mouseIsPressed)
+	{
+		if (getImageURL() != "-1")
+		{
+			p5ImageFile = getImageURL();
+		    image(p5ImageFile,200,50);
+		}
+	}
+
+	fill(125);
+	rect(10,200,100,40);
+	fill(255);
+	text("Sign in",30,230);
+
+
+
+
+	if (mouseX > 10 && mouseX < 10+100 && mouseY > 200 && mouseY < 200 + 40 && mouseIsPressed && btnSignIn == 0)
+	{
+		googleSignin();
+		btnSignIn = -1;
+	}
+
+
+}
+
+
+function mouseReleased()
+{
+	if (btnSignIn == -1)
+	{
+		btnSignIn = 0;
+	}
+}
