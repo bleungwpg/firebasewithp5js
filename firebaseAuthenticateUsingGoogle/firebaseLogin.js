0de97725c6469ff24fdc53f782b597b9cc7819ad
Ben Leung <benjamin.leung80@gmail.com>
2018-12-29 21:21:27 +0800

    Add files via upload

---
+++ firebaseAuthenticateUsingGoogle/firebaseLogin.js
@@ -0,0 +1,16 @@
+var firebaseApp;
+
+
+(function(){
+	// Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyBh258HDhov9mIVUjJRiCxNzf4-hmllZWU",
+		authDomain: "p5jstester.firebaseapp.com",
+		databaseURL: "https://p5jstester.firebaseio.com",
+		projectId: "p5jstester",
+		storageBucket: "p5jstester.appspot.com",
+		messagingSenderId: "294054023101"
+	};
+	firebaseApp = firebase.initializeApp(config);
+
+})
