2fd5ba1d60a524b6b0d88a2a65e492abbb57c161
Ben Leung <benjamin.leung80@gmail.com>
2018-12-31 22:28:44 +0800

    Add files via upload

--- firebaseAuthenticateUsingGoogle/firebaseMain.js
+++ firebaseAuthenticateUsingGoogle/firebaseMain.js
@@ -10,7 +10,20 @@ var provider;
 function setupFirebase()
 {
 
+	var user = firebase.auth().currentUser;
+	var name, email, photoUrl, uid, emailVerified;
 
+	if (user != null) {
+	  name = user.displayName;
+	  email = user.email;
+	  photoUrl = user.photoURL;
+	  emailVerified = user.emailVerified;
+	  uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+	                   // this value to authenticate with your backend server, if
+	                   // you have one. Use User.getToken() instead.
+		console.log('user: '+name);
+		console.log('email: '+email);
+	}
 
 
 	var config = {
@@ -28,12 +41,6 @@ function setupFirebase()
 
 	// reference to storage
 	storageRef = firebase.storage();
-
-
-	console.log('sign in using google');
-
-
-
 
 }
 
