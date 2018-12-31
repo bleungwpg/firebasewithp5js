261453999cb0ee577cf4d012fd225e24d741ea94
Ben Leung <benjamin.leung80@gmail.com>
2018-12-31 22:24:18 +0800

    Add files via upload

--- firebaseAuthenticateUsingGoogle/sketch.js
+++ firebaseAuthenticateUsingGoogle/sketch.js
@@ -35,21 +35,6 @@ function draw()
 		}
 	}
 
-	fill(125);
-	rect(10,200,100,40);
-	fill(255);
-	text("Sign in Redirect",30,230);
-
-
-
-
-	if (mouseX > 10 && mouseX < 10+100 && mouseY > 200 && mouseY < 200 + 40 && mouseIsPressed && btnSignIn == 0)
-	{
-		googleSignin();
-		btnSignIn = -1;
-	}
-
-
 }
 
 
